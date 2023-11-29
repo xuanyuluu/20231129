3871091e178a8fa53588cf5da69b86fc64ea4ba6
le402n17\uer <410381599@m365.fju.edu.tw>
2023-11-29 18:00:36 +0800

    畫四格圖

--- 20231129.R
+++ 20231129.R
@@ -2,3 +2,8 @@ dat = read.csv("Example data.csv", header = TRUE)
 head(dat)
 
 hist(dat[,"eGFR"])#畫直方圖
+par(mfrow = c(2, 2))
+hist(dat[,"eGFR"], col = "red")
+boxplot(dat[,"eGFR"], col = "blue")
+pie(table(dat[,"Education"]), col = c("blue", "red", "green"))
+barplot(table(dat[,"Education"]), col = c("gray90", "gray50", "gray10"))
